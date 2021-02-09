80e836d6925da872ef761b311daeb187cacd2911
jasonchg <jason.chg@outlook.com>
2021-02-10 03:44:33 +0800

    Fixed : change typo on the server.js

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,7 @@ app.use(errorHandler)
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static('frontend/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
   })
 }
 
